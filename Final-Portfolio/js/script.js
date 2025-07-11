ba3f4b567cf51b1fcea4d32820a8e61a1925498f
Natan <162522588+N4atan@users.noreply.github.com>
2025-07-11 11:35:03 -0300

    Add files via upload

---
+++ Final-Portfolio/js/script.js
@@ -0,0 +1,40 @@
+let lat, long;
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                lat = position.coords.latitude;
+                long = position.coords.longitude;
+                
+                getWeather();
+            },
+            function (error) {
+                alert("Erro ao obter localização: " + error.message);
+            }
+        );
+    } else {
+        alert("Geolocalização não é suportada neste navegador.");
+    }
+});
+
+function getWeather(){
+    if(lat == null || long == null) return;
+
+    console.log("Iniciando chamada API...");
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=119511082b57296fcbb32fec64eb0c53&units=metric`)
+    .then(response => response.json())
+    .then(data => setDataWeather(data))
+    .catch(error => console.error('Erro: ' + error))
+}
+
+function setDataWeather(data){
+    console.log(data);
+
+    document.getElementById('tempIcon').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
+    document.getElementById('tempTemp').innerHTML = data.main.temp.toFixed(0)
+    document.getElementById('tempCity').innerHTML = data.name;
+}
